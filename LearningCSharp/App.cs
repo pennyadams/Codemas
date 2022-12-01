08f6ecce44fe9c0b2c72a5ae4736aa45eecc4a5f
pennyadams <penny.adams@live.co.uk>
2022-12-01 14:33:55 +0000

    not working

--- LearningCSharp/App.cs
+++ LearningCSharp/App.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿/*
 namespace LearningCSharp
 {
-    internal class App
+    class App
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             List<Present> presents = new();
 
-            presents.Add(new Present("Easy Bake Oven", new List<string>{"Baking", "Kitchen"}));
+            presents.Add(new Present("Easy Bake Oven", new List<string> { "Baking", "Kitchen" }));
             presents.Add(new Present("Dinosour Toy", new List<string> { "Dinosaurs", "History", "Animals" }));
             presents.Add(new Present("Archeology Set", new List<string> { "Dinosaurs", "History" }));
             presents.Add(new Present("Lego Zoo", new List<string> { "Animals", "Lego" }));
@@ -23,7 +18,17 @@ namespace LearningCSharp
             Child Ella = new Child(9, "Ella", new List<string> { "Lego", "Animals" });
             Child Poppy = new Child(6, "Poppy", new List<string> { "History", "Kitchen", "Lego" });
 
-            Console.WriteLine(Tom.printPresents);
+            Console.WriteLine("Tom: ");
+            Tom.printPresents(presents);
+
+            Console.WriteLine("Ella: ");
+            Ella.printPresents(presents);
+
+            Console.WriteLine("Poppy: ");
+            Poppy.printPresents(presents);
         }
+
+
     }
 }
+*/
--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -1,46 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-//c# is a strongly typed language
-
-int count = 3;
-string name = "penny";
-bool isNaughty = false;
-
-
-int newCount = count * 3;
-newCount++;
-
-if (isNaughty)
+﻿namespace LearningCSharp
 {
-    Console.WriteLine("This child is naughty, give them coal");
-} else
-{
-    Console.WriteLine("This child is nice, give them presents");
+    class App
+    {
+        private static void Main(string[] args)
+        {
+            List<Present> presents = new();
+
+            presents.Add(new Present("Easy Bake Oven", new List<string> { "Baking", "Kitchen" }));
+            presents.Add(new Present("Dinosour Toy", new List<string> { "Dinosaurs", "History", "Animals" }));
+            presents.Add(new Present("Archeology Set", new List<string> { "Dinosaurs", "History" }));
+            presents.Add(new Present("Lego Zoo", new List<string> { "Animals", "Lego" }));
+            presents.Add(new Present("Unicorn Cake Mix", new List<string> { "Animals", "Baking", "Kitchen" }));
+            presents.Add(new Present("Lego House", new List<string> { "Lego", "Kitchen" }));
+
+            Child Tom = new Child(10, "Tom", new List<string> { "Dinosaurs", "Baking" });
+            Child Ella = new Child(9, "Ella", new List<string> { "Lego", "Animals" });
+            Child Poppy = new Child(6, "Poppy", new List<string> { "History", "Kitchen", "Lego" });
+
+            Console.WriteLine("Tom: ");
+            Tom.printPresents(presents);
+
+            Console.WriteLine("Ella: ");
+            Ella.printPresents(presents);
+
+            Console.WriteLine("Poppy: ");
+            Poppy.printPresents(presents);
+        }
+
+
+    }
 }
-
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine("Ho ");
-}
-
-List<string> presents = new List<string>
-{
-    "partridge",
-    "pear tree",
-    "five gold rings"
-};
-
-foreach (string gift in presents)
-{
-    Console.WriteLine(gift);
-}
-
-DateTime christmas = new DateTime(2022, 12, 25);
-double daysToGo = (christmas - (global::System.DateTime)DateTime.Now).TotalDays;
-Console.WriteLine($"Only {daysToGo} days until Christmas");
-
-
-
-
